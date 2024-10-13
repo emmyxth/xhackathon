6fdbeed162a58547a0aeb193baec154bf54766be
Emmy Thamakaison <93239069+emmyxth@users.noreply.github.com>
2024-10-12 18:52:55 -0700

    reverse mapping

--- src/components/EditableComponent.tsx
+++ src/components/EditableComponent.tsx
@@ -25,8 +25,6 @@ const example_items = [
 const categoryMapping = {
   PETS: "PETS",
   FOOD: "FOOD",
-  SHELF_1: "DECOR",
-  SHELF_2: "DECOR",
   CHAIR: "CHAIR",
   RUG: "RUG",
   POSTER1: "POSTER",
@@ -39,6 +37,16 @@ const categoryMapping = {
   GROUND1: "DECOR",
   CEILING: "GIF",
 };
+
+const reverseCategoriesDecor = [
+  "SHELF1",
+  "SHELF2",
+  "TABLE1",
+  "TABLE2",
+  "TABLE3",
+  "TABLE4",
+  "GROUND1",
+];
 
 interface Element {
   id: string;
@@ -53,8 +61,8 @@ interface Element {
 const categoryPositions = {
   PETS: { x: -20, y: 280 },
   FOOD: { x: 100, y: 130 },
-  SHELF_1: { x: 400, y: 100 },
-  SHELF_2: { x: 500, y: 150 },
+  SHELF_1: { x: 0, y: 0 },
+  SHELF_2: { x: 0, y: 0 },
   CHAIR: { x: 200, y: 220 },
   RUG: { x: 200, y: 300 },
   POSTER1: { x: 30, y: 20 },
@@ -112,11 +120,17 @@ const EditableComponent: React.FC = () => {
   };
 
   const addElement = (elementType: string, index: number) => {
-    const category =
-      categoryMapping[elementType as keyof typeof categoryMapping];
+    let category = categoryMapping[elementType as keyof typeof categoryMapping];
+
     const asset = getRandomAsset(category);
     if (!asset) return;
-
+    console.log("category: ", category);
+    if (category === "DECOR") {
+      const randomIndex = Math.floor(
+        Math.random() * reverseCategoriesDecor.length
+      );
+      category = reverseCategoriesDecor[randomIndex];
+    }
     const [name, src] = asset;
     const fileExtension = src.split(".").pop()?.toLowerCase();
     const type = fileExtension === "gif" ? "gif" : "image";

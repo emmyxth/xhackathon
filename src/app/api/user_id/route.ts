88a77431e3a591542e0fdf3005a8008b9cc5cfd6
Chris Hailey <chris.hailey8@gmail.com>
2024-11-14 19:36:08 -0800

    Add error handling for @

--- src/app/api/user_id/route.ts
+++ src/app/api/user_id/route.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
-  const user_handle = searchParams.get('user_handle')
+  let user_handle = searchParams.get('user_handle')
   const bearer_token = searchParams.get('bearer_token')
+  if (user_handle && user_handle.startsWith("@")) {
+    user_handle = user_handle.substring(1);
+  }
 
   const url = `https://api.twitter.com/2/users/by?usernames=${user_handle}`;
 
